d4ef83b0dee979a65462ad8951920be5a32bc988
linyimeng <linyimeng88@gmail.com>
2017-03-13 19:53:52 +0800

    纠正错误操作

--- fanfou/apps/wechat/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from fanfou.apps.common.generic import BM
-# Create your models here.
-class Wechat(BM):
-    appid = models.CharField(max_length=60)
-    appsecret = models.CharField(max_length=60)
-    token = models.CharField(max_length=60)
-    access_token = models.CharField(max_length=60)
--- fanfou/apps/wechat/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import include, url
-from fanfou.apps.wechat.views import wechat_main, get_access_token
-urlpatterns = [
-    url(r'^index/$',wechat_main),
-    url(r'^access_token/$', get_access_token),
-]
